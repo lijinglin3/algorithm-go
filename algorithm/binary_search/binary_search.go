94183499c8048ed00f441008b6f5d1fc6badcd4b
Eric Lee <li670149669@qq.com>
2019-08-21 23:16:31 +0800

    add binary search test

--- algorithm/binary_search/binary_search.go
+++ algorithm/binary_search/binary_search.go
@@ -41,14 +41,12 @@ func bs(nums []int, v int, low, high int) int {
 	mid := (low + high) / 2
 
 	switch {
-	case nums[mid] == v:
-		return mid
 	case nums[mid] > v:
 		return bs(nums, v, low, mid-1)
 	case nums[mid] < v:
 		return bs(nums, v, mid+1, high)
 	default:
-		return -1
+		return mid
 	}
 }
 
@@ -104,68 +102,5 @@ func BinarySearchLast(nums []int, v int) int {
 	return -1
 }
 
-// BinarySearchFirstGT 查找第一个大于等于给定值的元素
-func BinarySearchFirstGT(nums []int, v int) int {
-	length := len(nums)
-	if length == 0 {
-		return -1
-	}
-
-	low, high := 0, length-1
-
-	for low <= high {
-		mid := (low + high) >> 1
-
-		switch {
-		case nums[mid] == v:
-			if mid != length-1 && nums[mid+1] > v {
-				return mid + 1
-			}
-			low = mid + 1
-		case nums[mid] > v:
-			high = mid - 1
-		case nums[mid] < v:
-			low = mid + 1
-		}
-	}
-
-	for low <= high {
-		mid := (high + low) >> 1
-		if nums[mid] > v {
-			high = mid - 1
-		} else if nums[mid] < v {
-			low = mid + 1
-		} else {
-
-		}
-	}
-
-	return -1
-}
-
-// BinarySearchLastLT 查找最后一个小于等于给定值的元素
-func BinarySearchLastLT(nums []int, v int) int {
-	length := len(nums)
-	if length == 0 {
-		return -1
-	}
-
-	low, high := 0, length-1
-	for low <= high {
-		mid := (low + high) >> 1
-
-		switch {
-		case nums[mid] == v:
-			if mid != 0 && nums[mid-1] < v {
-				return mid - 1
-			}
-			high = mid - 1
-		case nums[mid] > v:
-			high = mid - 1
-		case nums[mid] < v:
-			low = mid + 1
-		}
-	}
-
-	return -1
-}
+// TODO 查找第一个大于等于给定值的元素
+// TODO 查找最后一个小于等于给定值的元素
--- algorithm/binary_search/binary_search_test.go
+++ algorithm/binary_search/binary_search_test.go
@@ -1,1 +1,65 @@
 package binary_search
+
+import (
+	"testing"
+)
+
+var (
+	testSlice0 = make([]int, 0)
+	testSlice1 = []int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+	testSlice2 = []int{10, 30, 30, 30, 50, 80, 80, 80, 90, 100}
+)
+
+func TestBinarySearch(t *testing.T) {
+	if BinarySearch(testSlice0, 0) != -1 {
+		t.Fail()
+	}
+	if BinarySearch(testSlice1, 0) != -1 {
+		t.Fail()
+	}
+	if BinarySearch(testSlice1, 30) != 2 {
+		t.Fail()
+	}
+}
+
+func TestBinarySearchRecursive(t *testing.T) {
+	if BinarySearchRecursive(testSlice0, 0) != -1 {
+		t.Fail()
+	}
+	if BinarySearchRecursive(testSlice1, 0) != -1 {
+		t.Fail()
+	}
+	if BinarySearchRecursive(testSlice1, 30) != 2 {
+		t.Fail()
+	}
+}
+
+func TestBinarySearchFirst(t *testing.T) {
+	if BinarySearchFirst(testSlice0, 0) != -1 {
+		t.Fail()
+	}
+	if BinarySearchFirst(testSlice2, 0) != -1 {
+		t.Fail()
+	}
+	if BinarySearchFirst(testSlice2, 30) != 1 {
+		t.Fail()
+	}
+	if BinarySearchFirst(testSlice2, 80) != 5 {
+		t.Fail()
+	}
+}
+
+func TestBinarySearchLast(t *testing.T) {
+	if BinarySearchLast(testSlice0, 0) != -1 {
+		t.Fail()
+	}
+	if BinarySearchLast(testSlice2, 0) != -1 {
+		t.Fail()
+	}
+	if BinarySearchLast(testSlice2, 30) != 3 {
+		t.Fail()
+	}
+	if BinarySearchLast(testSlice2, 80) != 7 {
+		t.Fail()
+	}
+}
