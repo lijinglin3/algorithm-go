6e085ab0e7b64298027e1dcabd83c8ff2779d93e
lijinglin <me@lijinglin.dev>
2020-02-03 17:18:29 +0800

    add leetcode 19

---
+++ leetcode/0019.remove-nth-node-from-end-of-list/remove_nth_node_from_end_of_list.go
@@ -0,0 +1,30 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func RemoveNthFromEnd(head *ListNode, n int) *ListNode {
+	p1, p2 := head, head
+	for i := 0; i < n; i++ {
+		p2 = p2.Next
+	}
+
+	// p2 为 nil 时表示删除的是第一个节点
+	if p2 == nil {
+		return head.Next
+	}
+
+	for {
+		p2 = p2.Next
+		if p2 == nil {
+			break
+		}
+		p1 = p1.Next
+	}
+
+	p1.Next = p1.Next.Next
+
+	return head
+}
---
+++ leetcode/0019.remove-nth-node-from-end-of-list/remove_nth_node_from_end_of_list_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func TestRemoveNthFromEnd(t *testing.T) {
+
+}
