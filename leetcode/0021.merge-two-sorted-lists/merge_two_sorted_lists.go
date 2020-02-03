ddeafcf725fd7ad9212615412629ba1e567dc962
lijinglin <me@lijinglin.dev>
2020-02-03 21:20:28 +0800

    add leetcode 21

---
+++ leetcode/0021.merge-two-sorted-lists/merge_two_sorted_lists.go
@@ -0,0 +1,34 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func MergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	result, tmp := &ListNode{}, &ListNode{}
+	cur := result
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			tmp = l1
+			l1 = l1.Next
+		} else {
+			tmp = l2
+			l2 = l2.Next
+		}
+
+		tmp.Next = nil
+		cur.Next = tmp
+		cur = cur.Next
+	}
+
+	if l1 == nil {
+		cur.Next = l2
+	}
+
+	if l2 == nil {
+		cur.Next = l1
+	}
+
+	return result.Next
+}
---
+++ leetcode/0021.merge-two-sorted-lists/merge_two_sorted_lists_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func TestMergeTwoLists(t *testing.T) {
+
+}
