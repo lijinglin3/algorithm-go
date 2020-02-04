ef4e46600030369c2c919ebb3b3e873cdc5c4e22
lijinglin <me@lijinglin.dev>
2020-02-04 21:08:47 +0800

    add leetcode 125

---
+++ leetcode/0125.valid-palindrome/valid_palindrome.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import (
+	"regexp"
+	"strings"
+)
+
+func IsPalindrome(s string) bool {
+	reg := regexp.MustCompile("[^0-9a-zA-Z]+")
+	s = strings.ToLower(reg.ReplaceAllString(s, ""))
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
+}
---
+++ leetcode/0125.valid-palindrome/valid_palindrome_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func TestIsPalindrome(t *testing.T) {
+
+}
