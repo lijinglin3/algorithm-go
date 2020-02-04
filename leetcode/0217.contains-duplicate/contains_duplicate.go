cae566dba3703551651f65fe8c9d0935668da262
lijinglin <me@lijinglin.dev>
2020-02-04 11:49:40 +0800

    add leetcode 217

---
+++ leetcode/0217.contains-duplicate/contains_duplicate.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func ContainsDuplicate(nums []int) bool {
+	m := make(map[int]interface{})
+	for _, n := range nums {
+		if _, exist := m[n]; exist {
+			return true
+		}
+		m[n] = struct{}{}
+	}
+	return false
+}
---
+++ leetcode/0217.contains-duplicate/contains_duplicate_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func TestContainsDuplicate(t *testing.T) {
+
+}
