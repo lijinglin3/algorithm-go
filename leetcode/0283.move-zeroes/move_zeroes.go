dbc5ee52b4c58467e983c106f0bbb11872b8e050
lijinglin <me@lijinglin.dev>
2020-02-04 19:16:23 +0800

    add leetcode 283

---
+++ leetcode/0283.move-zeroes/move_zeroes.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func MoveZeroes(nums []int) {
+	count := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			count++
+		} else if count > 0 {
+			nums[i-count] = nums[i]
+			nums[i] = 0
+		}
+	}
+}
---
+++ leetcode/0283.move-zeroes/move_zeroes_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func TestMoveZeroes(t *testing.T) {
+
+}
