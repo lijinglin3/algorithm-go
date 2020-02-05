c7e355f575e3719c374067480478239ae335dd01
lijinglin <me@lijinglin.dev>
2020-02-05 22:53:45 +0800

    add leetcode 98

---
+++ leetcode/0098.validate-binary-search-tree/validate_binary_search_tree.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import "math"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func IsValidBST(root *TreeNode) bool {
+	min := math.MinInt64
+	stack := make([]*TreeNode, 0)
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if root.Val <= min {
+			return false
+		}
+		min = root.Val
+		root = root.Right
+	}
+	return true
+}
---
+++ leetcode/0098.validate-binary-search-tree/validate_binary_search_tree_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func TestIsValidBST(t *testing.T) {
+
+}
