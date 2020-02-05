a3f51fa51349ec6c5487f5ecf8e59b2debf79b7a
lijinglin <me@lijinglin.dev>
2020-02-05 19:12:47 +0800

    add leetcode 104

---
+++ leetcode/0104.maximum-depth-of-binary-tree/maximum_depth_of_binary_tree.go
@@ -0,0 +1,79 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func MaxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := MaxDepth(root.Left)
+	right := MaxDepth(root.Right)
+
+	if left > right {
+		return left + 1
+	} else {
+		return right + 1
+	}
+}
+
+func MaxDepthByBFS(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	depth := 0
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		depth++
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+	return depth
+}
+
+func MaxDepthByDFS(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	type myNode struct {
+		node  *TreeNode
+		depth int
+	}
+
+	stack := []*myNode{{node: root, depth: 1}}
+	maxDepth := 0
+
+	for len(stack) > 0 {
+		n := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		node := n.node
+		depth := n.depth
+		if depth > maxDepth {
+			maxDepth = depth
+		}
+
+		if node.Right != nil {
+			stack = append(stack, &myNode{node: node.Right, depth: depth + 1})
+		}
+		if node.Left != nil {
+			stack = append(stack, &myNode{node: node.Left, depth: depth + 1})
+		}
+	}
+
+	return maxDepth
+}
---
+++ leetcode/0104.maximum-depth-of-binary-tree/maximum_depth_of_binary_tree_test.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import "testing"
+
+func TestMaxDepth(t *testing.T) {
+
+}
+
+func TestMaxDepthByBFS(t *testing.T) {
+
+}
+
+func TestMaxDepthByDFS(t *testing.T) {
+
+}
