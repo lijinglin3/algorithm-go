83830266352407fd0feebfe20c1f1fa93adde09b
lijinglin <me@lijinglin.dev>
2020-02-06 00:28:01 +0800

    add leetcode 412

---
+++ leetcode/0412.fizz-buzz/fizz_buzz.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import "strconv"
+
+func FizzBuzz(n int) []string {
+	result := make([]string, 0)
+	for i := 1; i <= n; i++ {
+		str := ""
+		if i%3 == 0 {
+			str += "Fizz"
+		}
+		if i%5 == 0 {
+			str += "Buzz"
+		}
+		if str == "" {
+			str = strconv.FormatInt(int64(i), 10)
+		}
+		result = append(result, str)
+	}
+	return result
+}
