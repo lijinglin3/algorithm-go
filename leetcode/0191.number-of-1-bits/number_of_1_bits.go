1b62fc4eabb3231a62160813a3014bacd999a43e
lijinglin <me@lijinglin.dev>
2020-02-06 14:01:20 +0800

    add leetcode 191

---
+++ leetcode/0191.number-of-1-bits/number_of_1_bits.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func HammingWeight(num uint32) int {
+	count := 0
+	for num != 0 {
+		if num%2 == 1 {
+			count++
+		}
+		num = num / 2
+	}
+	return count
+}
---
+++ leetcode/0191.number-of-1-bits/number_of_1_bits_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func TestHammingWeight(t *testing.T) {
+
+}
