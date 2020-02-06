01cd19431e2df44c738de141287f120ffd5e07a3
lijinglin <me@lijinglin.dev>
2020-02-06 14:45:48 +0800

    add leetcode 461

---
+++ leetcode/0461.hamming-distance/hamming_distance.go
@@ -0,0 +1,40 @@
+package leetcode
+
+func HammingDistance(x int, y int) int {
+	rx, ry := make([]int, 0), make([]int, 0)
+	for x != 0 {
+		rx = append(rx, x%2)
+		x = x / 2
+	}
+	for y != 0 {
+		ry = append(ry, y%2)
+		y = y / 2
+	}
+
+	lenx, leny := len(rx), len(ry)
+	count := 0
+	min := 0
+	if lenx > leny {
+		min = leny
+		for i := leny; i < lenx; i++ {
+			if rx[i] == 1 {
+				count++
+			}
+		}
+	} else {
+		min = lenx
+		for i := lenx; i < leny; i++ {
+			if ry[i] == 1 {
+				count++
+			}
+		}
+	}
+
+	for i := 0; i < min; i++ {
+		if rx[i] != ry[i] {
+			count++
+		}
+	}
+
+	return count
+}
