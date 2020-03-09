ba535b868c003659e150b7837ff0280dff749729
lijinglin <me@lijinglin.dev>
2020-03-09 21:19:07 +0800

    complete best_time_to_buy_and_sell_stock_ii_test.go

--- leetcode/0122.best-time-to-buy-and-sell-stock-ii/best_time_to_buy_and_sell_stock_ii_test.go
+++ leetcode/0122.best-time-to-buy-and-sell-stock-ii/best_time_to_buy_and_sell_stock_ii_test.go
@@ -1,7 +1,24 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type s []int
 
 func TestMaxProfit(t *testing.T) {
+	cases := [][]int{
+		{},
+		{1},
+		{7, 1, 5, 3, 6, 4},
+		{1, 2, 3, 4, 5},
+		{7, 6, 4, 3, 1},
+	}
+	values := []int{0, 0, 7, 4, 0}
 
+	for i, s := range cases {
+		assert.Equal(t, MaxProfit(s), values[i])
+	}
 }
