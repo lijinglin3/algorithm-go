2f1bb9b12a1e633f09d3f46ea188eb999d58cb6a
lijinglin <me@lijinglin.dev>
2020-03-09 21:28:27 +0800

    complete leetcode/0189.rotate-array/rotate_array_test.go

--- leetcode/0189.rotate-array/rotate_array_test.go
+++ leetcode/0189.rotate-array/rotate_array_test.go
@@ -1,7 +1,26 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestRotate(t *testing.T) {
+	cases := [][]int{
+		{1, 2, 3, 4, 5, 6, 7},
+		{-1, -100, 3, 99},
+		{1},
+	}
+	steps := []int{3, 2, 10}
+	results := [][]int{
+		{5, 6, 7, 1, 2, 3, 4},
+		{3, 99, -1, -100},
+		{1},
+	}
 
+	for i := range cases {
+		Rotate(cases[i], steps[i])
+		assert.Equal(t, cases[i], results[i])
+	}
 }
