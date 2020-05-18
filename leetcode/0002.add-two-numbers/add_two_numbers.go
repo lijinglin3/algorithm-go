cb323f30501bee058b81371211957200bee5eec3
lijinglin <me@lijinglin.dev>
2020-05-18 22:52:38 +0800

    add leetcode 2

---
+++ leetcode/0002.add-two-numbers/add_two_numbers.go
@@ -0,0 +1,41 @@
+package leetcode
+
+import . "github.com/lijinglin2019/algorithm-go/leetcode"
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1.Next == nil && l1.Val == 0 {
+		return l2
+	}
+	if l2.Next == nil && l2.Val == 0 {
+		return l1
+	}
+
+	flag := 0
+	result := &ListNode{}
+	cur := result
+	for l1 != nil || l2 != nil {
+		var val int
+		switch {
+		case l1 != nil && l2 != nil:
+			val = l1.Val + l2.Val + flag
+			l1, l2 = l1.Next, l2.Next
+		case l1 == nil && l2 != nil:
+			val = l2.Val + flag
+			l2 = l2.Next
+		case l1 != nil && l2 == nil:
+			val = l1.Val + flag
+			l1 = l1.Next
+		}
+		if val >= 10 {
+			val, flag = val-10, 1
+		} else {
+			flag = 0
+		}
+		cur.Next = &ListNode{Val: val}
+		cur = cur.Next
+	}
+	if flag == 1 {
+		cur.Next = &ListNode{Val: flag}
+	}
+	return result.Next
+}
---
+++ leetcode/0002.add-two-numbers/add_two_numbers_test.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/lijinglin2019/algorithm-go/leetcode"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddTwoNumbers(t *testing.T) {
+	example1 := "[1, 2, 3, 4, 5, 6, 7, 8, 9]"
+	example2 := "[1, 2, 3, 4, 5, 6, 7, 8]"
+	zero := "[0]"
+
+	result := "[2, 4, 6, 8, 0, 3, 5, 7, 0, 1]"
+	assert.Equal(t, leetcode.NewListNode(result),
+		addTwoNumbers(leetcode.NewListNode(example1), leetcode.NewListNode(example2)),
+	)
+
+	assert.Equal(t, leetcode.NewListNode(result),
+		addTwoNumbers(leetcode.NewListNode(example2), leetcode.NewListNode(example1)),
+	)
+
+	assert.Equal(t, leetcode.NewListNode(example1),
+		addTwoNumbers(leetcode.NewListNode(example1), leetcode.NewListNode(zero)),
+	)
+	assert.Equal(t, leetcode.NewListNode(example1),
+		addTwoNumbers(leetcode.NewListNode(zero), leetcode.NewListNode(example1)),
+	)
+}
