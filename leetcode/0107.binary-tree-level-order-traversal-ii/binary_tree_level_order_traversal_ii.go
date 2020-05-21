b87913209a93fd6a897dfe208816fbce21b9101b
lijinglin <me@lijinglin.dev>
2020-05-22 00:18:48 +0800

    add leetcode 107

---
+++ leetcode/0107.binary-tree-level-order-traversal-ii/binary_tree_level_order_traversal_ii.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import . "github.com/lijinglin2019/algorithm-go/leetcode"
+
+func levelOrderBottom(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+
+	result := make([][]int, 0)
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		size := len(queue)
+		tmp := make([]int, 0)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			tmp = append(tmp, node.Val)
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		result = append([][]int{tmp}, result...)
+	}
+	return result
+}
---
+++ leetcode/0107.binary-tree-level-order-traversal-ii/binary_tree_level_order_traversal_ii_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/lijinglin2019/algorithm-go/leetcode"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLevelOrderBottom(t *testing.T) {
+	cases := []*leetcode.TreeNode{
+		leetcode.NewTreeNode("[1, 2, 3, 4, 5]"),
+		{},
+		nil,
+	}
+	results := [][][]int{
+		{{4, 5}, {2, 3}, {1}},
+		{{0}},
+		nil,
+	}
+
+	for i := range cases {
+		assert.Equal(t, results[i], levelOrderBottom(cases[i]))
+	}
+}
