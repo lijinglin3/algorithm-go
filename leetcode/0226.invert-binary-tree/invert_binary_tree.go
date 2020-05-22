cadddf0cb1cbd48d28cc3ff70866c457525806be
lijinglin <me@lijinglin.dev>
2020-05-23 00:05:10 +0800

    add leetcode 226

---
+++ leetcode/0226.invert-binary-tree/invert_binary_tree.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import . "github.com/lijinglin2019/algorithm-go/leetcode"
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil || (root.Left == nil && root.Right == nil) {
+		return root
+	}
+
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+
+	root.Left, root.Right = right, left
+	return root
+}
