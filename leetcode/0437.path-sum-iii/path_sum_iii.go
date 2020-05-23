f3341587b87e14c53cbdd6af3bb0cfa5be4f5067
lijinglin <me@lijinglin.dev>
2020-05-24 00:45:02 +0800

    add leetcode 437

---
+++ leetcode/0437.path-sum-iii/path_sum_iii.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import . "github.com/lijinglin2019/algorithm-go/leetcode"
+
+var count map[int]int
+
+func pathSum(root *TreeNode, sum int) int {
+	count = map[int]int{0: 1}
+	return dfs(root, sum, 0)
+}
+
+func dfs(root *TreeNode, sum int, prefix int) int {
+	if root == nil {
+		return 0
+	}
+	res := 0
+	prefix += root.Val
+	res += count[prefix-sum]
+	count[prefix]++
+	res += dfs(root.Left, sum, prefix) + dfs(root.Right, sum, prefix)
+	count[prefix]--
+	return res
+}
---
+++ leetcode/0437.path-sum-iii/path_sum_iii_test.go
@@ -0,0 +1,12 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/lijinglin2019/algorithm-go/leetcode"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPathSum(t *testing.T) {
+	assert.Equal(t, 3, pathSum(leetcode.NewTreeNode("[10,5,-3,3,2,null,11,3,-2,null,1]"), 8))
+}
