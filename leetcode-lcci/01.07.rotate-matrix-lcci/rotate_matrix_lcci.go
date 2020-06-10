d769bedb3db997a92052f8cad3aeb664fd5c35b3
lijinglin <me@lijinglin.dev>
2020-06-11 01:24:30 +0800

    add leetcode lcci 01.07

---
+++ leetcode-lcci/01.07.rotate-matrix-lcci/rotate_matrix_lcci.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func rotate(matrix [][]int)  {
+	length := len(matrix)
+	if length <= 1 {
+		return
+	}
+
+	for times := 0; times <= length/2; times++ {
+		l := length - 2*times
+		for i := 0; i < l-1; i++ {
+			tmp := matrix[times][times+i]
+			matrix[times][times+i] = matrix[l+times-i-1][times]
+			matrix[l+times-i-1][times] = matrix[l+times-1][l+times-i-1]
+			matrix[l+times-1][l+times-i-1] = matrix[times+i][l+times-1]
+			matrix[times+i][l+times-1] = tmp
+		}
+	}
+}
---
+++ leetcode-lcci/01.07.rotate-matrix-lcci/rotate_matrix_lcci_test.go
@@ -0,0 +1,53 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRotate(t *testing.T) {
+	cases := [][][]int{
+		{
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9},
+		},
+		{
+			{5, 1, 9, 11},
+			{2, 4, 8, 10},
+			{13, 3, 6, 7},
+			{15, 14, 12, 16},
+		},
+		{
+			{1},
+		},
+		{
+			{},
+		},
+	}
+	results := [][][]int{
+		{
+			{7, 4, 1},
+			{8, 5, 2},
+			{9, 6, 3},
+		},
+		{
+			{15, 13, 2, 5},
+			{14, 3, 4, 1},
+			{12, 6, 8, 9},
+			{16, 7, 10, 11},
+		},
+		{
+			{1},
+		},
+		{
+			{},
+		},
+	}
+
+	for i := range cases {
+		rotate(cases[i])
+		assert.Equal(t, results[i], cases[i])
+	}
+}
