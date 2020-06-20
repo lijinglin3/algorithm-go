71a00b055d2da28b72508d2e21a3c84d4d79a51a
lijinglin <me@lijinglin.dev>
2020-06-21 02:11:34 +0800

    fix unit test

--- leetcode/0008.string-to-integer-atoi/string_to_integer_atoi_test.go
+++ leetcode/0008.string-to-integer-atoi/string_to_integer_atoi_test.go
@@ -1,7 +1,13 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMyAtoi(t *testing.T) {
-	myAtoi("    -43   22")
+	assert.Equal(t, -43, myAtoi("    -43   22"))
+	assert.Equal(t, -12, myAtoi("-12+34"))
+	assert.Equal(t, 0, myAtoi("abc"))
 }
--- leetcode/0747.largest-number-at-least-twice-of-others/largest_number_at_least_twice_of_others_test.go
+++ leetcode/0747.largest-number-at-least-twice-of-others/largest_number_at_least_twice_of_others_test.go
@@ -8,5 +8,6 @@ import (
 
 func TestDominantIndex(t *testing.T) {
 	assert.Equal(t, 1, dominantIndex([]int{3, 6, 1, 0}))
+	assert.Equal(t, 1, dominantIndex([]int{1, 8, 4, 2}))
 	assert.Equal(t, -1, dominantIndex([]int{1, 2, 3, 4}))
 }
