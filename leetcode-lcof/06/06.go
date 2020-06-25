a0a2eff81f8dc8e7bcf85044bfb51e0d180fc971
lijinglin <me@lijinglin.dev>
2020-06-26 02:09:44 +0800

    add leetcode lcof 6

---
+++ leetcode-lcof/06/06.go
@@ -0,0 +1,19 @@
+package leetcode
+
+import . "github.com/lijinglin2019/algorithm-go/leetcode"
+
+func reversePrint(head *ListNode) []int {
+	result := make([]int, 0)
+	for head != nil {
+		result = append(result, head.Val)
+		head = head.Next
+	}
+	length := len(result) - 1
+	if length <= 0 {
+		return result
+	}
+	for i := 0; i <= length/2; i++ {
+		result[i], result[length-i] = result[length-i], result[i]
+	}
+	return result
+}
---
+++ leetcode-lcof/06/06_test.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/lijinglin2019/algorithm-go/leetcode"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReversePrint(t *testing.T) {
+	assert.Equal(t, []int{}, reversePrint(nil))
+	assert.Equal(t, []int{}, reversePrint(leetcode.NewListNode("[]")))
+	assert.Equal(t, []int{4, 5, 2, 3, 1}, reversePrint(leetcode.NewListNode("[1,3,2,5,4]")))
+	assert.Equal(t, []int{6, 4, 5, 2, 3, 1}, reversePrint(leetcode.NewListNode("[1,3,2,5,4,6]")))
+}
