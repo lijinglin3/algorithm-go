d0765c8f61abc59fd89f54610e430719b123791e
lijinglin <me@lijinglin.dev>
2020-06-27 01:43:26 +0800

    update leetcode 191

--- leetcode/0191.number-of-1-bits/number_of_1_bits.go
+++ leetcode/0191.number-of-1-bits/number_of_1_bits.go
@@ -3,10 +3,8 @@ package leetcode
 func hammingWeight(num uint32) int {
 	count := 0
 	for num != 0 {
-		if num%2 == 1 {
-			count++
-		}
-		num = num / 2
+		num &= num - 1
+		count++
 	}
 	return count
 }
