833b91c0cf92084d374c8724d36f057e96090849
lijinglin <me@lijinglin.dev>
2020-06-27 22:09:22 +0800

    add leetcode lcof 28

---
+++ leetcode-lcof/28/28.go
@@ -0,0 +1,19 @@
+package leetcode
+
+import . "github.com/lijinglin2019/algorithm-go/leetcode"
+
+func isSymmetric(root *TreeNode) bool {
+	return isMirror(root, root)
+}
+
+func isMirror(root1, root2 *TreeNode) bool {
+	if root1 == nil && root2 == nil {
+		return true
+	}
+	if root1 == nil || root2 == nil {
+		return false
+	}
+	return (root1.Val == root2.Val) &&
+		isMirror(root1.Left, root2.Right) &&
+		isMirror(root1.Right, root2.Left)
+}
---
+++ leetcode-lcof/28/28_test.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/lijinglin2019/algorithm-go/leetcode"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsSymmetric(t *testing.T) {
+	cases := []*leetcode.TreeNode{
+		leetcode.NewTreeNode("[1, 2, 3, 4, 5]"),
+		leetcode.NewTreeNode("[1, 2, 2, 3, null, 4]"),
+		leetcode.NewTreeNode("[1, 2, 2, 3, 4, 4, 3]"),
+		{},
+		nil,
+	}
+	results := []bool{
+		false,
+		false,
+		true,
+		true,
+		true,
+	}
+
+	for i := range cases {
+		assert.Equal(t, results[i], isSymmetric(cases[i]))
+	}
+}
