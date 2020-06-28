9c08fb52e3a1b6e7a1ca376c8e5146fe3bd2c3ff
lijinglin <me@lijinglin.dev>
2020-06-28 21:51:54 +0800

    add leetcode lcof 33

---
+++ leetcode-lcof/33/33_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/lijinglin2019/algorithm-go/leetcode"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLevelOrder(t *testing.T) {
+	cases := []*leetcode.TreeNode{
+		leetcode.NewTreeNode("[1, 2, 3, 4, 5]"),
+		{},
+		nil,
+	}
+	results := [][][]int{
+		{{1}, {2, 3}, {4, 5}},
+		{{0}},
+		{},
+	}
+
+	for i := range cases {
+		assert.Equal(t, results[i], levelOrder(cases[i]))
+	}
+}
