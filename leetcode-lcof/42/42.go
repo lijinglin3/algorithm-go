a2c4922fe9efb28b543dcbdda313e5f5670f7e92
lijinglin <me@lijinglin.dev>
2020-06-30 22:28:37 +0800

    add leetcode lcof 42

---
+++ leetcode-lcof/42/42.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func maxSubArray(nums []int) int {
+	sum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] > 0 {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > sum {
+			sum = nums[i]
+		}
+	}
+	return sum
+}
