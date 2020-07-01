204c2804744a35734da141516bf1c6809d299f6b
lijinglin <me@lijinglin.dev>
2020-07-01 22:42:26 +0800

    add leetcode lcof 52

---
+++ leetcode-lcof/52/52.go
@@ -0,0 +1,36 @@
+package leetcode
+
+import . "github.com/lijinglin2019/algorithm-go/leetcode"
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	len1, len2 := 0, 0
+	p1, p2 := headA, headB
+	for p1 != nil {
+		len1++
+		p1 = p1.Next
+	}
+	for p2 != nil {
+		len2++
+		p2 = p2.Next
+	}
+
+	p1, p2 = headA, headB
+	if len1 > len2 {
+		for i := 0; i < len1-len2; i++ {
+			p1 = p1.Next
+		}
+	} else {
+		for i := 0; i < len2-len1; i++ {
+			p2 = p2.Next
+		}
+	}
+
+	for p1 != nil || p2 != nil {
+		if p1 == p2 {
+			return p1
+		}
+
+		p1, p2 = p1.Next, p2.Next
+	}
+	return nil
+}
---
+++ leetcode-lcof/52/52_test.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/lijinglin2019/algorithm-go/leetcode"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetIntersectionNode(t *testing.T) {
+	list1 := leetcode.NewListNode("[1, 2, 3]")
+	list2 := leetcode.NewListNode("[1, 2, 3, 4]")
+	assert.Equal(t, (*leetcode.ListNode)(nil), getIntersectionNode(list1, list2))
+
+	common := leetcode.NewListNode("[7, 8, 9]")
+	assert.Equal(t, common, getIntersectionNode(common, common))
+	assert.Equal(t, (*leetcode.ListNode)(nil), getIntersectionNode(common, (*leetcode.ListNode)(nil)))
+
+	list1.Last().Next, list2.Last().Next = common, common
+	assert.Equal(t, common, getIntersectionNode(list1, common))
+	assert.Equal(t, common, getIntersectionNode(list1, list2))
+	assert.Equal(t, common, getIntersectionNode(list2, list1))
+}
