1d83182311e056bbb0442473925ae99d10f73c45
lijinglin <me@lijinglin.dev>
2020-07-04 22:40:00 +0800

    add leetcode lcof 55-1

---
+++ leetcode-lcof/55-1/55.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import . "github.com/lijinglin2019/algorithm-go/leetcode"
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+
+	if left > right {
+		return left + 1
+	}
+	return right + 1
+}
