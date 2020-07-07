709c7f96f105ee9ba306e9a37738e7e135dc3117
lijinglin <me@lijinglin.dev>
2020-07-07 22:39:08 +0800

    fix format

--- leetcode-lcof/15/15_test.go
+++ leetcode-lcof/15/15_test.go
@@ -7,6 +7,6 @@ import (
 )
 
 func TestHammingWeight(t *testing.T) {
-	assert.Equal(t, 3, hammingWeight(0b00000000000000000000000000001011))
-	assert.Equal(t, 31, hammingWeight(0b11111111111111111111111111111101))
+	assert.Equal(t, 3, hammingWeight(11))
+	assert.Equal(t, 31, hammingWeight(4294967293))
 }
--- leetcode/0445.add-two-numbers-ii/add_two_numbers_ii.go
+++ leetcode/0445.add-two-numbers-ii/add_two_numbers_ii.go
@@ -2,28 +2,6 @@ package leetcode
 
 import . "github.com/lijinglin2019/algorithm-go/leetcode"
 
-//func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-//	num1, num2 := 0, 0
-//	for l1 != nil {
-//		num1 = num1*10 + l1.Val
-//		l1 = l1.Next
-//	}
-//	for l2 != nil {
-//		num2 = num2*10 + l2.Val
-//		l2 = l2.Next
-//	}
-//
-//	sum := num1 + num2
-//	result := &ListNode{Val: sum % 10}
-//	sum = sum / 10
-//
-//	for sum != 0 {
-//		result = &ListNode{Val: sum % 10, Next: result}
-//		sum = sum / 10
-//	}
-//	return result
-//}
-
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l1.Next == nil && l1.Val == 0 {
 		return l2
@@ -32,19 +10,10 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		return l1
 	}
 
-	var s1, s2 []int
-	for l1 != nil {
-		s1 = append(s1, l1.Val)
-		l1 = l1.Next
-	}
-	for l2 != nil {
-		s2 = append(s2, l2.Val)
-		l2 = l2.Next
-	}
-
-	flag := 0
-	result := (*ListNode)(nil)
+	s1, s2 := toSlice(l1), toSlice(l2)
 	len1, len2 := len(s1), len(s2)
+	result, flag := (*ListNode)(nil), 0
+
 	for len1 != 0 || len2 != 0 {
 		val := 0
 		switch {
@@ -70,4 +39,12 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		result = &ListNode{Val: flag, Next: result}
 	}
 	return result
+}
+
+func toSlice(l *ListNode) (result []int) {
+	for l != nil {
+		result = append(result, l.Val)
+		l = l.Next
+	}
+	return
 }
