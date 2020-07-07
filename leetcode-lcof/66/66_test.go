07f6a8476d42451a4fa33abf42a585a66e4a332d
lijinglin <me@lijinglin.dev>
2020-07-07 22:41:01 +0800

    fix unit test

--- leetcode-lcof/66/66_test.go
+++ leetcode-lcof/66/66_test.go
@@ -7,5 +7,7 @@ import (
 )
 
 func TestConstructArr(t *testing.T) {
+	assert.Equal(t, []int{}, constructArr([]int{}))
+	assert.Equal(t, []int{1}, constructArr([]int{5}))
 	assert.Equal(t, []int{120, 60, 40, 30, 24}, constructArr([]int{1, 2, 3, 4, 5}))
 }
