f7803655b318e5b37f3e1bacdecd24f29151a019
lijinglin <me@lijinglin.dev>
2020-07-08 22:32:55 +0800

    add leetcode lcof 7

---
+++ leetcode-lcof/07/07.go
@@ -0,0 +1,19 @@
+package leetcode
+
+import . "github.com/lijinglin2019/algorithm-go/leetcode"
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{Val: preorder[0]}
+	i := 0
+	for ; i < len(inorder); i++ {
+		if inorder[i] == preorder[0] {
+			break
+		}
+	}
+	root.Left = buildTree(preorder[1:len(inorder[:i])+1], inorder[:i])
+	root.Right = buildTree(preorder[len(inorder[:i])+1:], inorder[i+1:])
+	return root
+}
---
+++ leetcode-lcof/07/07_test.go
@@ -0,0 +1,12 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/lijinglin2019/algorithm-go/leetcode"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildTree(t *testing.T) {
+	assert.Equal(t, leetcode.NewTreeNode("[3,9,20,null,null,15,7]"), buildTree([]int{3, 9, 20, 15, 7}, []int{9, 3, 15, 20, 7}))
+}
