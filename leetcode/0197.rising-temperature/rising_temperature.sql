68277daea2568a78b0a8fead8b31b66afdd7948f
lijinglin <me@lijinglin.dev>
2020-08-03 01:09:23 +0800

    add sql test case

--- leetcode/0197.rising-temperature/rising_temperature.sql
+++ leetcode/0197.rising-temperature/rising_temperature.sql
@@ -1,5 +1,22 @@
+Create table If Not Exists Weather
+(
+    Id          int,
+    RecordDate  date,
+    Temperature int
+);
+Truncate table Weather;
+insert into Weather (Id, RecordDate, Temperature)
+values ('1', '2015-01-01', '10');
+insert into Weather (Id, RecordDate, Temperature)
+values ('2', '2015-01-02', '25');
+insert into Weather (Id, RecordDate, Temperature)
+values ('3', '2015-01-03', '20');
+insert into Weather (Id, RecordDate, Temperature)
+values ('4', '2015-01-04', '30');
+
+# Write your MySQL query statement below
 SELECT B.Id
 FROM Weather A,
      Weather B
 WHERE DATEDIFF(B.RecordDate, A.RecordDate) = 1
-  AND A.Temperature < B.Temperature
+  AND A.Temperature < B.Temperature;
