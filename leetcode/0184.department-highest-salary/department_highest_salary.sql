e8cd0f6fcd02121957afadf667101ae336f62d44
lijinglin <me@lijinglin.dev>
2020-08-03 01:05:58 +0800

    add leetcode 184

---
+++ leetcode/0184.department-highest-salary/department_highest_salary.sql
@@ -0,0 +1,34 @@
+Create table If Not Exists Employee
+(
+    Id           int,
+    Name         varchar(255),
+    Salary       int,
+    DepartmentId int
+);
+Create table If Not Exists Department
+(
+    Id   int,
+    Name varchar(255)
+);
+Truncate table Employee;
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('1', 'Joe', '70000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('2', 'Jim', '90000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('3', 'Henry', '80000', '2');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('4', 'Sam', '60000', '2');
+insert into Employee (Id, Name, Salary, DepartmentId)
+values ('5', 'Max', '90000', '1');
+Truncate table Department;
+insert into Department (Id, Name)
+values ('1', 'IT');
+insert into Department (Id, Name)
+values ('2', 'Sales');
+
+# Write your MySQL query statement below
+SELECT Department.name AS 'Department', Employee.name AS 'Employee', Salary
+FROM Employee
+         JOIN Department ON Employee.DepartmentId = Department.Id
+WHERE (Employee.DepartmentId, Salary) IN (SELECT DepartmentId, MAX(Salary) FROM Employee GROUP BY DepartmentId);
