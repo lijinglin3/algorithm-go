ae01044f9d0d6df00c9eabb267b1b123bb97bcaa
lijinglin <me@lijinglin.dev>
2020-08-03 00:58:55 +0800

    add leetcode 181

---
+++ leetcode/0181.employees-earning-more-than-their-managers/employees_earning_more_than_their_managers.sql
@@ -0,0 +1,23 @@
+Create table If Not Exists Employee
+(
+    Id        int,
+    Name      varchar(255),
+    Salary    int,
+    ManagerId int
+);
+Truncate table Employee;
+insert into Employee (Id, Name, Salary, ManagerId)
+values ('1', 'Joe', '70000', '3');
+insert into Employee (Id, Name, Salary, ManagerId)
+values ('2', 'Henry', '80000', '4');
+insert into Employee (Id, Name, Salary, ManagerId)
+values ('3', 'Sam', '60000', 'None');
+insert into Employee (Id, Name, Salary, ManagerId)
+values ('4', 'Max', '90000', 'None');
+
+# Write your MySQL query statement below
+SELECT a.NAME AS Employee
+FROM Employee AS a
+         JOIN Employee AS b
+              ON a.ManagerId = b.Id
+                  AND a.Salary > b.Salary;
