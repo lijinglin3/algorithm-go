a9608a9366ee3788be5ff88c602af4621a15f113
lijinglin <me@lijinglin.dev>
2020-08-03 00:45:10 +0800

    add leetcode 178

---
+++ leetcode/0178.rank-scores/rank_scores.sql
@@ -0,0 +1,22 @@
+Create table If Not Exists Scores
+(
+    Id    int,
+    Score DECIMAL(3, 2)
+);
+Truncate table Scores;
+insert into Scores (Id, Score)
+values ('1', '3.5');
+insert into Scores (Id, Score)
+values ('2', '3.65');
+insert into Scores (Id, Score)
+values ('3', '4.0');
+insert into Scores (Id, Score)
+values ('4', '3.85');
+insert into Scores (Id, Score)
+values ('5', '4.0');
+insert into Scores (Id, Score)
+values ('6', '3.65');
+
+# Write your MySQL query statement below
+SELECT Score, DENSE_RANK() OVER (ORDER BY Score DESC ) AS `Rank`
+FROM Scores;
