5f60265e951cfb4d44c4f8c23050b1e33144ce58
lijinglin <me@lijinglin.dev>
2020-08-03 00:52:41 +0800

    add sql test case

--- leetcode/0176.second-highest-salary/second_highest_salary.sql
+++ leetcode/0176.second-highest-salary/second_highest_salary.sql
@@ -1,1 +1,16 @@
-SELECT IFNULL((SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1), NULL) AS SecondHighestSalary
+Create table If Not Exists Employee
+(
+    Id     int,
+    Salary int
+);
+Truncate table Employee;
+insert into Employee (Id, Salary)
+values ('1', '100');
+insert into Employee (Id, Salary)
+values ('2', '200');
+insert into Employee (Id, Salary)
+values ('3', '300');
+
+# Write your MySQL query statement below
+SELECT IFNULL((SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1),
+              NULL) AS SecondHighestSalary;
--- leetcode/0177.nth-highest-salary/nth_highest_salary.sql
+++ leetcode/0177.nth-highest-salary/nth_highest_salary.sql
@@ -1,3 +1,17 @@
+Create table If Not Exists Employee
+(
+    Id     int,
+    Salary int
+);
+Truncate table Employee;
+insert into Employee (Id, Salary)
+values ('1', '100');
+insert into Employee (Id, Salary)
+values ('2', '200');
+insert into Employee (Id, Salary)
+values ('3', '300');
+
+# Write your MySQL query statement below
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
     RETURN (
