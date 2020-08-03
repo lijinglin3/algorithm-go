4cad40e652d8ac1a98865c850110cb9aa4096268
lijinglin <me@lijinglin.dev>
2020-08-03 22:31:09 +0800

    add leetcode 512

---
+++ leetcode/0512.game-play-analysis-ii/game_play_analysis_ii.sql
@@ -0,0 +1,27 @@
+Create table If Not Exists Activity
+(
+    player_id    int,
+    device_id    int,
+    event_date   date,
+    games_played int
+);
+Truncate table Activity;
+insert into Activity (player_id, device_id, event_date, games_played)
+values ('1', '2', '2016-03-01', '5');
+insert into Activity (player_id, device_id, event_date, games_played)
+values ('1', '2', '2016-05-02', '6');
+insert into Activity (player_id, device_id, event_date, games_played)
+values ('2', '3', '2017-06-25', '1');
+insert into Activity (player_id, device_id, event_date, games_played)
+values ('3', '1', '2016-03-02', '0');
+insert into Activity (player_id, device_id, event_date, games_played)
+values ('3', '4', '2018-07-03', '5');
+
+# Write your MySQL query statement below
+SELECT A.player_id, A.device_id
+FROM Activity A,
+     (SELECT player_id, MIN(event_date) AS first_login
+      FROM Activity
+      GROUP BY player_id) AS B
+WHERE A.event_date = B.first_login
+  AND A.player_id = B.player_id
