5ae44ab438dd9dd9e265c59d4cfb47cc3e72fc7a
lijinglin <me@lijinglin.dev>
2020-08-06 00:51:48 +0800

    add leetcode 584

---
+++ leetcode/0584.find-customer-referee/find_customer_referee.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS customer
+(
+    id         INT,
+    name       VARCHAR(25),
+    referee_id INT
+);
+Truncate table customer;
+insert into customer (id, name, referee_id)
+values ('1', 'Will', 'None');
+insert into customer (id, name, referee_id)
+values ('2', 'Jane', 'None');
+insert into customer (id, name, referee_id)
+values ('3', 'Alex', '2');
+insert into customer (id, name, referee_id)
+values ('4', 'Bill', 'None');
+insert into customer (id, name, referee_id)
+values ('5', 'Zack', '1');
+insert into customer (id, name, referee_id)
+values ('6', 'Mark', '2');
+
+# Write your MySQL query statement below
+SELECT name
+FROM customer
+WHERE referee_id != 2
+   OR referee_id IS NULL
