2caa60a229833d776c8d8a999e3c79e4a24889f0
lijinglin <me@lijinglin.dev>
2020-10-09 17:09:50 +0800

    update leetcode 263

--- leetcode/0263.ugly-number/ugly_number.go
+++ leetcode/0263.ugly-number/ugly_number.go
@@ -1,30 +1,17 @@
 package leetcode
 
 func isUgly(num int) bool {
-	if num <= 0 {
+	if num < 1 {
 		return false
 	}
-
-	for num != 1 {
-		count := 0
-		if num%2 == 0 {
-			num /= 2
-		} else {
-			count++
-		}
-		if num%3 == 0 {
-			num /= 3
-		} else {
-			count++
-		}
-		if num%5 == 0 {
-			num /= 5
-		} else {
-			count++
-		}
-		if count == 3 {
-			return false
-		}
+	for num%2 == 0 {
+		num /= 2
 	}
-	return true
+	for num%3 == 0 {
+		num /= 3
+	}
+	for num%5 == 0 {
+		num /= 5
+	}
+	return num == 1
 }
