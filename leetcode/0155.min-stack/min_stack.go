d96ea467640a4c381e1b5c59ba378ddbed54f2e0
lijinglin <me@lijinglin.dev>
2021-03-01 22:31:51 +0800

    update leetcode 155

--- leetcode/0155.min-stack/min_stack.go
+++ leetcode/0155.min-stack/min_stack.go
@@ -2,58 +2,48 @@ package leetcode
 
 // MinStack MinStack
 type MinStack struct {
-	stack, sort []int
-	length      int
+	stack, min []int
+	length     int
 }
 
 // Constructor initialize your data structure here.
 func Constructor() MinStack {
-	return MinStack{stack: make([]int, 0), sort: make([]int, 0), length: 0}
+	return MinStack{stack: make([]int, 0), min: make([]int, 0), length: 0}
 }
 
 // Push 入栈
-func (stack *MinStack) Push(x int) {
-	stack.length = stack.length + 1
-	stack.stack = append(stack.stack, x)
-	stack.sort = append(stack.sort, x)
-	for i := stack.length - 2; i >= 0; i-- {
-		if stack.sort[i+1] > stack.sort[i] {
-			stack.sort[i+1] = stack.sort[i]
-			stack.sort[i] = x
-		} else {
-			break
-		}
+func (s *MinStack) Push(x int) {
+	s.stack = append(s.stack, x)
+	if s.length == 0 || s.min[s.length-1] > x {
+		s.min = append(s.min, x)
+	} else {
+		s.min = append(s.min, s.min[s.length-1])
 	}
+	s.length = s.length + 1
 }
 
 // Pop 出栈
-func (stack *MinStack) Pop() {
-	if stack.length == 0 {
+func (s *MinStack) Pop() {
+	if s.length == 0 {
 		return
 	}
-	val := stack.stack[stack.length-1]
-	for i, v := range stack.sort {
-		if v == val {
-			stack.sort = append(stack.sort[:i], stack.sort[i+1:]...)
-			break
-		}
-	}
-	stack.stack = stack.stack[:stack.length-1]
-	stack.length = stack.length - 1
+	s.stack = s.stack[:s.length-1]
+	s.min = s.min[:s.length-1]
+	s.length = s.length - 1
 }
 
 // Top 查询栈顶元素
-func (stack *MinStack) Top() int {
-	if stack.length == 0 {
+func (s *MinStack) Top() int {
+	if s.length == 0 {
 		return 0
 	}
-	return stack.stack[stack.length-1]
+	return s.stack[s.length-1]
 }
 
 // GetMin 返回栈中的最小值
-func (stack *MinStack) GetMin() int {
-	if stack.length == 0 {
+func (s *MinStack) GetMin() int {
+	if s.length == 0 {
 		return 0
 	}
-	return stack.sort[stack.length-1]
+	return s.min[s.length-1]
 }
