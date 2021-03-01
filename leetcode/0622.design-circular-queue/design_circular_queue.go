3b102b531c68debeb42ba6396e1bc3478e29c3e6
lijinglin <me@lijinglin.dev>
2021-03-01 21:47:04 +0800

    update leetcode 622

--- leetcode/0622.design-circular-queue/design_circular_queue.go
+++ leetcode/0622.design-circular-queue/design_circular_queue.go
@@ -2,14 +2,14 @@ package leetcode
 
 // MyCircularQueue ...
 type MyCircularQueue struct {
-	queue []int
-	size  int
+	head, count, size int
+	queue             []int
 }
 
 // Constructor initialize your data structure here. Set the size of the queue to be k
 func Constructor(k int) MyCircularQueue {
 	return MyCircularQueue{
-		queue: make([]int, 0, k),
+		queue: make([]int, k),
 		size:  k,
 	}
 }
@@ -19,7 +19,8 @@ func (q *MyCircularQueue) EnQueue(value int) bool {
 	if q.IsFull() {
 		return false
 	}
-	q.queue = append(q.queue, value)
+	q.queue[(q.head+q.count)%q.size] = value
+	q.count++
 	return true
 }
 
@@ -28,7 +29,8 @@ func (q *MyCircularQueue) DeQueue() bool {
 	if q.IsEmpty() {
 		return false
 	}
-	q.queue = q.queue[1:]
+	q.head = (q.head + 1) % q.size
+	q.count--
 	return true
 }
 
@@ -37,7 +39,7 @@ func (q *MyCircularQueue) Front() int {
 	if q.IsEmpty() {
 		return -1
 	}
-	return q.queue[0]
+	return q.queue[q.head]
 }
 
 // Rear get the last item from the queue
@@ -45,15 +47,15 @@ func (q *MyCircularQueue) Rear() int {
 	if q.IsEmpty() {
 		return -1
 	}
-	return q.queue[len(q.queue)-1]
+	return q.queue[(q.head+q.count-1)%q.size]
 }
 
 // IsEmpty checks whether the circular queue is empty or not
 func (q *MyCircularQueue) IsEmpty() bool {
-	return len(q.queue) == 0
+	return q.count == 0
 }
 
 // IsFull checks whether the circular queue is full or not
 func (q *MyCircularQueue) IsFull() bool {
-	return len(q.queue) >= q.size
+	return q.count == q.size
 }
