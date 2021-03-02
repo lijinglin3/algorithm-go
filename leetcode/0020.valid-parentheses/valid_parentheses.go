3d3266e2e4c05454944ccc78c761d829766d9583
lijinglin <me@lijinglin.dev>
2021-03-02 23:24:38 +0800

    update leetcode 20

--- leetcode/0020.valid-parentheses/valid_parentheses.go
+++ leetcode/0020.valid-parentheses/valid_parentheses.go
@@ -1,23 +1,14 @@
 package leetcode
 
 func isValid(s string) bool {
-	stack := make([]rune, 0)
+	stack, index := make([]rune, len(s)), 0
 	for _, v := range s {
-		if len(stack) == 0 {
-			stack = append(stack, v)
-			continue
-		}
-		head := stack[len(stack)-1]
-		switch {
-		case string(head) == "{" && string(v) == "}":
-			stack = stack[:len(stack)-1]
-		case string(head) == "[" && string(v) == "]":
-			stack = stack[:len(stack)-1]
-		case string(head) == "(" && string(v) == ")":
-			stack = stack[:len(stack)-1]
-		default:
-			stack = append(stack, v)
+		if index != 0 && ((stack[index-1] == '{' && v == '}') || (stack[index-1] == '[' && v == ']') || (stack[index-1] == '(' && v == ')')) {
+			index--
+		} else {
+			stack[index] = v
+			index++
 		}
 	}
-	return len(stack) == 0
+	return index == 0
 }
