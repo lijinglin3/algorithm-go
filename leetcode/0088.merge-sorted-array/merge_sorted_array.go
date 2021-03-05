fe521726d4c4ecb46c8db073c86a03a18f968f3b
lijinglin <me@lijinglin.dev>
2021-03-05 23:23:33 +0800

    add leetcode 88

---
+++ leetcode/0088.merge-sorted-array/merge_sorted_array.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for p := m + n; m > 0 && n > 0; p-- {
+		if nums2[n-1] >= nums1[m-1] {
+			nums1[p-1] = nums2[n-1]
+			n--
+		} else {
+			nums1[p-1] = nums1[m-1]
+			m--
+		}
+	}
+	for ; n-1 >= 0; n-- {
+		nums1[n-1] = nums2[n-1]
+	}
+}
