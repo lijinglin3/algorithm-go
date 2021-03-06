1631c5421aead84d16f32c15c4a8d74ae69e133d
lijinglin <me@lijinglin.dev>
2021-03-06 20:33:15 +0800

    update leetcode 283

--- leetcode/0283.move-zeroes/move_zeroes.go
+++ leetcode/0283.move-zeroes/move_zeroes.go
@@ -1,13 +1,10 @@
 package leetcode
 
 func moveZeroes(nums []int) {
-	count := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == 0 {
-			count++
-		} else if count > 0 {
-			nums[i-count] = nums[i]
-			nums[i] = 0
+	for i, j := 0, 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			j++
 		}
 	}
 }
