cf7a271c325a79b5a24975235803695808e29315
lijinglin <me@lijinglin.dev>
2021-03-07 14:34:30 +0800

    update leetcode 162

--- leetcode/0162.find-peak-element/find_peak_element.go
+++ leetcode/0162.find-peak-element/find_peak_element.go
@@ -1,14 +1,14 @@
 package leetcode
 
 func findPeakElement(nums []int) int {
-	left, right := 0, len(nums)-1
-	for left < right {
-		mid := left + (right-left)/2
+	l, r := 0, len(nums)-1
+	for l < r {
+		mid := (l + r) / 2
 		if nums[mid] > nums[mid+1] {
-			right = mid
+			r = mid
 		} else {
-			left = mid + 1
+			l = mid + 1
 		}
 	}
-	return left
+	return l
 }
