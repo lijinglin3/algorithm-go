fca838ae5b85830ba9c46945f512dc6f1b1dbe20
lijinglin <me@lijinglin.dev>
2022-01-10 22:12:20 +0800

    update leetcode 155

--- leetcode/0155.min-stack/min_stack.go
+++ leetcode/0155.min-stack/min_stack.go
@@ -2,24 +2,23 @@ package leetcode
 
 // MinStack MinStack
 type MinStack struct {
-	stack, min []int
-	length     int
+	stack  [][2]int
+	length int
 }
 
 // Constructor initialize your data structure here.
 func Constructor() MinStack {
-	return MinStack{stack: make([]int, 0), min: make([]int, 0), length: 0}
+	return MinStack{stack: make([][2]int, 0), length: 0}
 }
 
 // Push 入栈
 func (s *MinStack) Push(x int) {
-	s.stack = append(s.stack, x)
-	if s.length == 0 || s.min[s.length-1] > x {
-		s.min = append(s.min, x)
-	} else {
-		s.min = append(s.min, s.min[s.length-1])
+	min := x
+	if s.length > 0 && s.GetMin() < x {
+		min = s.GetMin()
 	}
-	s.length = s.length + 1
+	s.stack = append(s.stack, [2]int{x, min})
+	s.length++
 }
 
 // Pop 出栈
@@ -28,7 +27,6 @@ func (s *MinStack) Pop() {
 		return
 	}
 	s.stack = s.stack[:s.length-1]
-	s.min = s.min[:s.length-1]
 	s.length = s.length - 1
 }
 
@@ -37,7 +35,7 @@ func (s *MinStack) Top() int {
 	if s.length == 0 {
 		return 0
 	}
-	return s.stack[s.length-1]
+	return s.stack[s.length-1][0]
 }
 
 // GetMin 返回栈中的最小值
@@ -45,5 +43,5 @@ func (s *MinStack) GetMin() int {
 	if s.length == 0 {
 		return 0
 	}
-	return s.min[s.length-1]
+	return s.stack[s.length-1][1]
 }
